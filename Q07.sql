d02d895853732b227378a74b18840e4b8006d11d
Sveloz03 <velozsam@gmail.com>
2026-06-01 15:58:10 -0400

    q07

---
+++ Q07.sql
@@ -0,0 +1,7 @@
+use northwind;
+
+# 7.What are the products that we carry where we have no units on hand, but 1 or 
+#   more units of them on backorder?  Order them by product name. 
+
+SELECT * FROM products
+
