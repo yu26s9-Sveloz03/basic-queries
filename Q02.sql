d0a46b2a2d9212a7b9e471b0c24f1d6e08e2e5bd
Sveloz03 <velozsam@gmail.com>
2026-06-01 15:05:40 -0400

    Q02

---
+++ Q02.sql
@@ -0,0 +1,11 @@
+use northwind;
+
+# 2. Write a query to list the product id, product name, and unit price of every 
+#    product. 
+
+SELECT 
+	productID,
+    productName,
+    UnitPrice
+FROM
+	products
