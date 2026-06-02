17980ee818c8a3ee203e7a1ad79822fe7984963c
Sveloz03 <velozsam@gmail.com>
2026-06-01 21:57:16 -0400

    Q09

---
+++ Q09.sql
@@ -0,0 +1,8 @@
+use northwind;
+
+# 9. Write a query that lists all of the columns and all the rows of the categories table?  
+#	 What is the category id of seafood? 
+
+SELECT * FROM categories
+
+# Category id of seafood is 8
